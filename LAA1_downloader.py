2ba2c1cb2aa929c89e0df47310a61b65f51c0f85
Gavin <gleng.email@gmail.com>
2015-11-05 09:06:59 +0000

    add pkey

--- LAA1_downloader.py
+++ LAA1_downloader.py
@@ -14,9 +14,10 @@ import re
 import argparse
 import json
 import datetime
+import hashlib
 
 # url = "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/410395/SFR36_2014_LA_tables_revised.xlsx"
-# output_path = "" 
+# output_path = "tempLAA1.csv"
 # sheet = "LAA1"
 # required_indicators = ["2011", "2012", "2013", "2014"]
 
@@ -25,30 +26,18 @@ def download(url, sheet, reqFields, outPath):
     yearReq = reqFields
     dName = outPath
 
-    col = ['ecode', 'name', 'year', 'rate']
-  
-    try:
-        socket = urllib.request.urlopen(url)
-    except urllib.error.HTTPError as e:
-        errfile.write(str(now()) + ' excel download HTTPError is ' + str(e.code) + ' . End progress\n')
-        logfile.write(str(now()) + ' error and end progress\n')
-        sys.exit('excel download HTTPError = ' + str(e.code))
-    except urllib.error.URLError as e:
-        errfile.write(str(now()) + ' excel download URLError is ' + str(e.args) + ' . End progress\n')
-        logfile.write(str(now()) + ' error and end progress\n')
-        sys.exit('excel download URLError = ' + str(e.args))
-    except Exception:
-        print('excel file download error')
-        import traceback
-        errfile.write(str(now()) + ' generic exception: ' + str(traceback.format_exc()) + ' . End progress\n')
-        logfile.write(str(now()) + ' error and end progress\n')
-        sys.exit('generic exception: ' + traceback.format_exc())
-    
+    col = ['ecode', 'name', 'year', 'rate', 'pkey']
+
+    # open url
+    socket = openurl(url)
+
     #operate this excel file
     logfile.write(str(now()) + ' excel file loading\n')
+    print('excel file loading------')
     xd = pd.ExcelFile(socket)
     df = xd.parse(sheet)
 
+    # indicator checking
     logfile.write(str(now()) + ' indicator checking\n')
     print('indicator checking------')
     for i in range(df.shape[0]):
@@ -76,6 +65,7 @@ def download(url, sheet, reqFields, outPath):
     for j in col:
         raw_data[j] = []
 
+    # data reading
     logfile.write(str(now()) + ' data reading\n')
     print('data reading------')
     for i in range(restartIndex, df.shape[0]):
@@ -87,8 +77,19 @@ def download(url, sheet, reqFields, outPath):
                 raw_data[col[2]].append(yearReq[ii])
                 raw_data[col[3]].append(df.iloc[i, yearCol[ii]])
                 ii += 1
-    
+    logfile.write(str(now()) + ' data reading end\n')
+    print('data reading end------')
+
+    # create primary key by md5 for each row
+    logfile.write(str(now()) + ' create primary key\n')
+    print('create primary key------')
+    keyCol = [0, 2]
+    raw_data[col[-1]] = fpkey(raw_data, col, keyCol)
+    logfile.write(str(now()) + ' create primary key end\n')
+    print('create primary key end------')
+
     #save csv file
+    logfile.write(str(now()) + ' writing to file\n')
     print('writing to file ' + dName)
     dfw = pd.DataFrame(raw_data, columns=col)
     dfw.to_csv(dName, index=False)
@@ -96,6 +97,36 @@ def download(url, sheet, reqFields, outPath):
     print('Requested data has been extracted and saved as ' + dName)
     logfile.write(str(now()) + ' finished\n')
     print("finished")
+
+def openurl(url):
+    try:
+        socket = urllib.request.urlopen(url)
+        return socket
+    except urllib.error.HTTPError as e:
+        errfile.write(str(now()) + ' file download HTTPError is ' + str(e.code) + ' . End progress\n')
+        logfile.write(str(now()) + ' error and end progress\n')
+        sys.exit('file download HTTPError = ' + str(e.code))
+    except urllib.error.URLError as e:
+        errfile.write(str(now()) + ' file download URLError is ' + str(e.args) + ' . End progress\n')
+        logfile.write(str(now()) + ' error and end progress\n')
+        sys.exit('file download URLError = ' + str(e.args))
+    except Exception:
+        print('file download error')
+        import traceback
+        errfile.write(str(now()) + ' generic exception: ' + str(traceback.format_exc()) + ' . End progress\n')
+        logfile.write(str(now()) + ' error and end progress\n')
+        sys.exit('generic exception: ' + traceback.format_exc())
+
+def fpkey(data, col, keyCol):
+    mystring = ''
+    pkey = []
+    for i in range(len(data[col[0]])):
+        for j in keyCol:
+            mystring += str(data[col[j]][i])
+        mymd5 = hashlib.md5(mystring.encode()).hexdigest()
+        pkey.append(mymd5)
+
+    return pkey
 
 def now():
     return datetime.datetime.now()
